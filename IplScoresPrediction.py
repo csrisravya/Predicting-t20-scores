1151a517278e81d9e10c29fa0b6e60c5c9ae2c75
csrisravya <92312453+csrisravya@users.noreply.github.com>
2022-08-27 13:17:32 +0530

    Update IplScoresPrediction.py

--- IplScoresPrediction.py
+++ IplScoresPrediction.py
@@ -66,3 +66,30 @@ match_df=match_df[match_df['dl_applied']==0]
 
 match_df=match_df[['match_id','winner','total_runs']]
 
+# Now examining the status of the matches in the 2nd innings
+dlvr_df = match_df.merge(dlvr_df,on='match_id')
+dlvr_df=dlvr_df[dlvr_df['inning']==2]  # getting the dataframe for the 2nd inning
+dlvr_df['current_score'] = dlvr_df.groupby('match_id').cumsum()["total_runs_y"] # the cumulative i.e score of the batting team until the present delivery is stored in a new colums current_score
+
+dlvr_df['runs_left'] = dlvr_df['total_runs_x']-dlvr_df['current_score']
+dlvr_df['balls_left'] = 120 -((dlvr_df['over']-1)*6 + dlvr_df['ball'])
+
+# now we calculate at which ball the wicket was taken for the player
+dlvr_df['player_dismissed'] = dlvr_df['player_dismissed'].fillna("0") 
+dlvr_df['player_dismissed'] = dlvr_df['player_dismissed'].apply(lambda x:x if x == "0" else "1")
+dlvr_df['player_dismissed'] = dlvr_df['player_dismissed'].astype('int') 
+wkts = dlvr_df.groupby('match_id').cumsum()['player_dismissed'].values
+dlvr_df['wickets'] = 10 - wkts
+
+
+# calculating the present run rate 
+dlvr_df['curr_run_rate'] = (dlvr_df['current_score']*6)/(120-dlvr_df['balls_left'])
+
+# now we calculate the required run rate for the team to win the match
+
+dlvr_df['req_run_rate'] = (dlvr_df['runs_left']*6) / dlvr_df['balls_left']
+
+def match_won(entry):
+    return 1 if entry['batting_team']== entry['winner'] else 0
+dlvr_df['result']= dlvr_df.apply(match_won,axis=1)
+
